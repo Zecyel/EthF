2698d7ce2b6387e8e336b70ea6c0d11b3461a6a6
Zecyel <zecyel@163.com>
2023-10-27 14:12:03 +0800

    chore: 更新了Trigger传递事件的例程

--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,17 +1,22 @@
 import { Environment, Context, Trigger, Listener } from 'xevent'
 
-let env = new Environment(new Context())
+// let env = new Environment(new Context())
 
-env.next((_) => 5).debug()
+// env.next((_) => 5).debug()
 
 let ev = new Trigger((e) => {
     document
         .getElementById('button')
-        .addEventListener('click', () => {
-            e({})
+        .addEventListener('click', (ev) => {
+            e({
+                x: ev.clientX,
+                y: ev.clientY
+            })
         })
 })
 
-ev.addListener(new Listener((env_) => {
-    env_.next((_) => 123).debug()
+ev.addListener(new Listener((env) => {
+    env.next((_) => {
+        console.log(_.$.x, _.$.y)
+    }).next((_) => 123).debug()
 }))
