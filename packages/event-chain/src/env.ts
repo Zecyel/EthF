fbb65cc57c8652ae9ef161f16ba70c0122baca38
Zecyel <zecyel@163.com>
2023-11-03 11:25:07 +0800

    feat: 实现了useEnvironment插件

--- packages/event-chain/src/env.ts
+++ packages/event-chain/src/env.ts
@@ -12,14 +12,28 @@ export function createEnvironment(plugins: EnvironmentPlugin[]): EnvironmentBase
     return createInstance(EnvironmentBase, plugins)
 }
 
-let useEnvironment = new AppPlugin(
+export const useEnvironment = new AppPlugin(
     (base) => class extends base {
-        // TODO
+
+        // @ts-ignore
+        private env: {
+            [key: string]: EnvironmentBase
+        } = []
+
+        addEnv(name: string, env: EnvironmentBase): void {
+            this.env[name] = env
+        }
+        getEnv(name: string): EnvironmentBase {
+            if (! (name in this.env))
+                throw new Error(`Environment ${name} not found`)
+            return this.env[name]
+        }
     }
 )
 
 declare module '@ethf/core' {
     interface AppBase {
-        
+        addEnv(name: string, env: EnvironmentBase): void,
+        getEnv(name: string): EnvironmentBase
     }
 }
--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,41 +1,27 @@
-import { App, AppBase, AppPlugin, createApp } from "@ethf/core"
+import { createApp } from "@ethf/core";
+import { EnvironmentBase, EnvironmentPlugin, createEnvironment, useEnvironment } from "@ethf/event-chain";
 
-let plugin1 = new AppPlugin(
-    (base) => class extends base {
-        c: number = 1
-        debugC() {
-            console.log(`debug-C ${this.c}`)
+let app = createApp([ useEnvironment ])
+
+let env_plg = new EnvironmentPlugin(
+    base => class extends base {
+        count: number = 0
+        foo(): EnvironmentBase {
+            this.count ++
+            console.log(`Count: ${this.count}`)
+            return this
         }
     }
 )
 
-declare module '@ethf/core' {
-    interface AppBase {
-        c: number
-        debugC(): void
+declare module '@ethf/event-chain' {
+    interface EnvironmentBase {
+        count: number
+        foo(): EnvironmentBase
     }
 }
 
-let plugin2 = new AppPlugin(
-    (base) => class extends base {
-        d: number = 3
-        debugD() {
-            console.log(`debug-D ${this.d}`) 
-        }
-    }
-)
+let env = createEnvironment([ env_plg ])
+app.addEnv('counter', env)
 
-declare module '@ethf/core' {
-    interface AppBase {
-        d: number
-        debugD(): void
-    }
-}
-
-let app1 = createApp([ plugin1 ])
-
-let app2 = createApp([ plugin2 ])
-
-app1.debugC()
-app2.debugD()
-app2.debugC() // oh bro that's weird
+app.getEnv('counter').foo().foo().foo()
