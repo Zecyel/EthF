2115040062f72f26099350c26e51f30286fd1408
Zecyel <zecyel@163.com>
2023-11-10 08:50:39 +0800

    test: 新增了两个core的单元测试

--- packages/core/test/simple.test.ts
+++ packages/core/test/simple.test.ts
@@ -1,8 +1,10 @@
 import { describe, test, expect } from "@jest/globals"
-import { XCore, createInstance } from "../src/plugin"
+import { XCore, XPlugin, createInstance } from "../src/plugin"
 
 describe("Basic operations", () => {
+    
     class Base extends XCore {}
+
     const plugin1 = (base: new () => Base) => class extends base {
         a: number = 10
         increase() {
@@ -12,11 +14,43 @@ describe("Basic operations", () => {
             return this.a * this.a
         }
     }
-    const base = createInstance(Base, [ plugin1 ])
-    test('Test constructor, method and get', () => {
+
+    test('Test function createInstance', () => {
+        const base = createInstance(Base, [ plugin1 ])
         expect(base.a).toBe(10)
         base.increase()
         expect(base.a).toBe(11)
         expect(base.square).toBe(121)
+    })
+
+    function createBasePlugin<T>(plugin: XPlugin<Base, T>): XPlugin<Base, T> {
+        return plugin
+    }
+    
+    const plugin2 = createBasePlugin(
+        base => class extends base {
+            b: number = 20
+        }
+    )
+    test('Test function createPlugin', () => {
+        const base = createInstance(Base, [ plugin2 ])
+        expect(base.b).toBe(20)
+    })
+    
+    test('Test multiplugins', () => {
+        const base = createInstance(Base, [ plugin1, plugin2 ])
+        expect(base.a).toBe(10)
+        expect(base.b).toBe(20)
+    })
+    
+    const plugin3 = createBasePlugin(
+        base => class extends base {
+            a: number = 40
+        }
+    )
+
+    test('Test overlap plugins', () => {
+        const base = createInstance(Base, [ plugin1, plugin3 ])
+        expect(base.a).toBe(40)
     })
 })
