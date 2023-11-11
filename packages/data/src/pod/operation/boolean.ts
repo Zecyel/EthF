7c4b09793ca419dcf2826af18602ad83cbf480f5
Zecyel <zecyel@163.com>
2023-11-12 00:22:49 +0800

    feat: 实现了XBoolean的相关运算

--- packages/data/src/pod/operation/boolean.ts
+++ packages/data/src/pod/operation/boolean.ts
@@ -1,21 +1,17 @@
 import { XBoolean } from "../type/boolean"
-import { BinaryFunctionType, PODBinaryOperation, PODUnaryOperation, UnaryFunctionType } from "./operation"
+import { PODBinaryOperation, PODUnaryOperation } from "./operation"
 
-const BooleanBinaryOperation = (op: BinaryFunctionType<boolean>, lhs: XBoolean, rhs: XBoolean) =>
-    PODBinaryOperation<boolean, XBoolean>(
-        false,
-        XBoolean,
-        op, lhs, rhs
-    )
+const binaryOperationGenerator = 
+    (op: (lhs: boolean, rhs: boolean) => boolean) =>
+    (lhs: XBoolean, rhs: XBoolean) =>
+    PODBinaryOperation<boolean, boolean, boolean>(false, XBoolean, op, lhs, rhs)
 
-const BooleanUnaryOperation = (op: UnaryFunctionType<boolean>, arg: XBoolean) =>
-    PODUnaryOperation<boolean, XBoolean>(
-        false,
-        XBoolean,
-        op, arg
-    )
+export const And = binaryOperationGenerator((lhs, rhs) => lhs && rhs)
+export const Or = binaryOperationGenerator((lhs, rhs) => lhs || rhs)
 
-export const And: BinaryFunctionType<XBoolean> = (lhs, rhs) => BooleanBinaryOperation((lhs, rhs) => lhs && rhs, lhs, rhs)
-export const Or: BinaryFunctionType<XBoolean> = (lhs, rhs) => BooleanBinaryOperation((lhs, rhs) => lhs || rhs, lhs, rhs)
+const unaryOperationGenerator = 
+    (op: (arg: boolean) => boolean) =>
+    (arg: XBoolean) =>
+    PODUnaryOperation<boolean, boolean>(false, XBoolean, op, arg)
 
-export const Not: UnaryFunctionType<XBoolean> = (arg) => BooleanUnaryOperation((arg) => !arg, arg)
+export const Not = unaryOperationGenerator((arg) => !arg)
