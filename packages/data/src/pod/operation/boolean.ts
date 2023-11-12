6441f8786a3aa81ea265325272a388101ecda1a8
Zecyel <zecyel@163.com>
2023-11-12 20:30:27 +0800

    chore: 更改了data下的部分函数名并导出

--- packages/data/src/pod/operation/boolean.ts
+++ packages/data/src/pod/operation/boolean.ts
@@ -1,17 +1,17 @@
 import { XBoolean } from "../type/boolean"
 import { PODBinaryOperation, PODUnaryOperation } from "./operation"
 
-const binaryOperationGenerator = 
+export const booleanBinaryOperationGenerator = 
     (op: (lhs: boolean, rhs: boolean) => boolean) =>
     (lhs: XBoolean, rhs: XBoolean) =>
     PODBinaryOperation<boolean, boolean, boolean>(false, XBoolean, op, lhs, rhs)
 
-export const And = binaryOperationGenerator((lhs, rhs) => lhs && rhs)
-export const Or = binaryOperationGenerator((lhs, rhs) => lhs || rhs)
+export const And = booleanBinaryOperationGenerator((lhs, rhs) => lhs && rhs)
+export const Or = booleanBinaryOperationGenerator((lhs, rhs) => lhs || rhs)
 
-const unaryOperationGenerator = 
+export const booleanUnaryOperationGenerator = 
     (op: (arg: boolean) => boolean) =>
     (arg: XBoolean) =>
     PODUnaryOperation<boolean, boolean>(false, XBoolean, op, arg)
 
-export const Not = unaryOperationGenerator((arg) => !arg)
+export const Not = booleanUnaryOperationGenerator((arg) => !arg)
--- packages/data/src/pod/operation/number.ts
+++ packages/data/src/pod/operation/number.ts
@@ -2,30 +2,30 @@ import { XBoolean } from "../type/boolean"
 import { XNumber } from "../type/number"
 import { PODBinaryOperation, PODUnaryOperation } from "./operation"
 
-const binaryOperationGenerator = 
+export const numberBinaryOperationGenerator = 
     (op: (lhs: number, rhs: number) => number) =>
     (lhs: XNumber, rhs: XNumber) =>
     PODBinaryOperation<number, number, number>(0, XNumber, op, lhs, rhs)
 
-export const Add = binaryOperationGenerator((lhs, rhs) => lhs + rhs)
-export const Sub = binaryOperationGenerator((lhs, rhs) => lhs - rhs)
-export const Mul = binaryOperationGenerator((lhs, rhs) => lhs * rhs)
+export const Add = numberBinaryOperationGenerator((lhs, rhs) => lhs + rhs)
+export const Sub = numberBinaryOperationGenerator((lhs, rhs) => lhs - rhs)
+export const Mul = numberBinaryOperationGenerator((lhs, rhs) => lhs * rhs)
 
-const unaryOperationGenerator = 
+export const numberUnaryOperationGenerator = 
     (op: (arg: number) => number) =>
     (arg: XNumber) =>
     PODUnaryOperation<number, number>(0, XNumber, op, arg)
 
-export const Neg = unaryOperationGenerator((arg) => -arg)
+export const Neg = numberUnaryOperationGenerator((arg) => -arg)
 
-const toBoolBinaryOperationGenerator = 
+export const numberToBoolBinaryOperationGenerator = 
     (op: (lhs: number, rhs: number) => boolean) =>
     (lhs: XNumber, rhs: XNumber) =>
     PODBinaryOperation<number, number, boolean>(false, XBoolean, op, lhs, rhs)
 
-export const Equal = toBoolBinaryOperationGenerator((lhs, rhs) => lhs === rhs)
-export const NotEqual = toBoolBinaryOperationGenerator((lhs, rhs) => lhs !== rhs)
-export const Greater = toBoolBinaryOperationGenerator((lhs, rhs) => lhs > rhs)
-export const GreaterEqual = toBoolBinaryOperationGenerator((lhs, rhs) => lhs >= rhs)
-export const Less = toBoolBinaryOperationGenerator((lhs, rhs) => lhs < rhs)
-export const LessEqual = toBoolBinaryOperationGenerator((lhs, rhs) => lhs <= rhs)
+export const Equal = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs === rhs)
+export const NotEqual = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs !== rhs)
+export const Greater = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs > rhs)
+export const GreaterEqual = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs >= rhs)
+export const Less = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs < rhs)
+export const LessEqual = numberToBoolBinaryOperationGenerator((lhs, rhs) => lhs <= rhs)
