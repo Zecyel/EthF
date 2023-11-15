1a28f39a5bbdafb41119aa2eeed6ae2abda36770
Zecyel <zecyel@163.com>
2023-11-15 17:12:49 +0800

    feat: 实现了dom/useClass

---
+++ packages/dom/src/elements/base/class.ts
@@ -0,0 +1,13 @@
+import { createDOMBasePlugin } from "./dom"
+
+export const useClass = createDOMBasePlugin(
+    base => class extends base {
+        addClass(...cls: string[]) {
+            this.el.classList.add(...cls)
+        }
+
+        removeClass(...cls: string[]) {
+            this.el.classList.remove(...cls)
+        }
+    }
+)
