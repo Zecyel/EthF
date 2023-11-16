c80d47de4fd10b779aa4090134b826abe50ff21a
Zecyel <zecyel@163.com>
2023-11-16 10:12:56 +0800

    feat: 提高dom/useClass兼容性

--- packages/dom/src/elements/base/plugins/class.ts
+++ packages/dom/src/elements/base/plugins/class.ts
@@ -3,11 +3,11 @@ import { createDOMBasePlugin } from "../dom"
 export const useClass = createDOMBasePlugin(
     base => class extends base {
         addClass(...cls: string[]) {
-            this.el.classList.add(...cls)
+            this.el.classList.add(...cls.filter((v) => v !== ''))
         }
 
         removeClass(...cls: string[]) {
-            this.el.classList.remove(...cls)
+            this.el.classList.remove(...cls.filter((v) => v !== ''))
         }
     }
 )
